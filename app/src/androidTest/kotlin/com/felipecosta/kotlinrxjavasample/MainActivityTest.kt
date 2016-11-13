6ffaa759604b429c1b522c4f6b276bb019f1066c
fcostaa <felipe.h.j.costa@gmail.com>
2016-11-13 15:13:45 -0200

    Add some MainActivity test cases

---
+++ app/src/androidTest/kotlin/com/felipecosta/kotlinrxjavasample/MainActivityTest.kt
@@ -0,0 +1,44 @@
+package com.felipecosta.kotlinrxjavasample
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.DrawerActions.open
+import android.support.test.espresso.contrib.DrawerMatchers.isClosed
+import android.support.test.espresso.contrib.DrawerMatchers.isOpen
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.hamcrest.core.AllOf.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule @JvmField
+    var activityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun givenMainActivityFirstAppearedThenFirstSampleIsShowing() {
+        onView(allOf(withText("Sample 1"), isDescendantOfA(withId(R.id.main_content)))).
+                check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun whenSelectSample2ThenSecondSampleIsShowing() {
+
+        onView(withId(R.id.drawer_layout)).perform(open())
+
+        onView(withId(R.id.drawer_layout)).check(matches(isOpen()))
+
+        onView(allOf(withText("Sample 2"), isDescendantOfA(withId(R.id.drawer_layout)))).
+                perform(click())
+
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed()))
+
+        onView(allOf(withText("Sample 2"), isDescendantOfA(withId(R.id.main_content)))).
+                check(matches(isDisplayed()))
+    }
+}

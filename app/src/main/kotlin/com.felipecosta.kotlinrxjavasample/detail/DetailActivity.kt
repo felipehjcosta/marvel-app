f1a60c08b00d4be19a5ec6cf9d404ca5653db554
Bruno Kosawa <bruno.costa@olxbr.com>
2017-01-17 14:15:19 -0200

    Adding Detail Activity to the project

---
+++ app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/detail/DetailActivity.kt
@@ -0,0 +1,27 @@
+package com.felipecosta.kotlinrxjavasample.detail
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+import com.felipecosta.kotlinrxjavasample.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+}

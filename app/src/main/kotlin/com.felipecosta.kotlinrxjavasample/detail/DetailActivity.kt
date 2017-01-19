4b0590e1511ed76e8f432b6061b4539b8197bb75
Bruno Kosawa <bruno.costa@olxbr.com>
2017-01-19 18:27:43 -0200

    Setting title and description of the detail screen

--- app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/detail/DetailActivity.kt
+++ app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/detail/DetailActivity.kt
@@ -3,10 +3,9 @@ package com.felipecosta.kotlinrxjavasample.detail
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.widget.TextView
 import com.felipecosta.kotlinrxjavasample.R
 import com.felipecosta.kotlinrxjavasample.listing.model.marvel.Character
 
@@ -26,20 +25,29 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
-    lateinit var mCharacter: Character
+    lateinit var character: Character
+    lateinit var toolbar: Toolbar
+    lateinit var description: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        mCharacter = intent.extras.getParcelable<Character>(CHARACTER_OBJECT)
+        character = intent.extras.getParcelable<Character>(CHARACTER_OBJECT)
+        initView(character.name)
+        loadInfo()
     }
+
+    private fun initView(title: String) {
+        setContentView(R.layout.activity_detail)
+        toolbar = findViewById(R.id.toolbar) as Toolbar
+        toolbar.title = title
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        description = findViewById(R.id.text_description) as TextView
+    }
+
+    private fun loadInfo() {
+        description.text = character.description
+    }
+
 }

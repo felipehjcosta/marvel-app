301d97a0d8ed15cbd29d0d883bf9a40630f1d01d
Bruno Kosawa <bruno.costa@olxbr.com>
2017-01-19 14:06:58 -0200

    Making DetailActivity receive a bundle with character

--- app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/detail/DetailActivity.kt
+++ app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/detail/DetailActivity.kt
@@ -1,15 +1,32 @@
 package com.felipecosta.kotlinrxjavasample.detail
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
-
 import com.felipecosta.kotlinrxjavasample.R
+import com.felipecosta.kotlinrxjavasample.listing.model.marvel.Character
 
 class DetailActivity : AppCompatActivity() {
+
+    companion object Factory {
+        val CHARACTER_OBJECT: String? = "com.felipecosta.kotlinrxjavasample.CHARACTER_BUNDLE"
+
+        fun startDetail(activity: Activity, character: Character?) {
+            val bundle = Bundle()
+            bundle.putParcelable(CHARACTER_OBJECT, character)
+
+            val activityIntent = Intent(activity, DetailActivity::class.java)
+            activityIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            activityIntent.putExtras(bundle)
+            activity.startActivity(activityIntent)
+        }
+    }
+
+    lateinit var mCharacter: Character
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +40,6 @@ class DetailActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        mCharacter = intent.extras.getParcelable<Character>(Factory.CHARACTER_OBJECT)
     }
 }

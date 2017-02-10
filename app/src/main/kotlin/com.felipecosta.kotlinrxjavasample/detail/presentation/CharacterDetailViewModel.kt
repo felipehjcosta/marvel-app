9892229a039570c6318397580f0d44287db60246
Bruno Kosawa <bruno.costa@olxbr.com>
2017-02-10 13:22:36 -0200

    Creating observables to get details about character in CharacterDetailsViewModel

--- app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/detail/presentation/CharacterDetailViewModel.kt
+++ app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/detail/presentation/CharacterDetailViewModel.kt
@@ -1,5 +1,24 @@
 package com.felipecosta.kotlinrxjavasample.detail.presentation
 
-class CharacterDetailViewModel(val name: String, val description: String, val thumbnailURL: String) {
+import com.felipecosta.kotlinrxjavasample.data.pojo.Character
+import com.felipecosta.kotlinrxjavasample.rx.AsyncCommand
+import com.felipecosta.kotlinrxjavasample.rx.Command
+import io.reactivex.Observable
+
+class CharacterDetailViewModel(private val asyncCharacterCommand: AsyncCommand<Character>) {
+
+    private val characterObservable: Observable<Character> = asyncCharacterCommand.execution.share()
+
+    val name: Observable<String>
+        get() = characterObservable.map { it.name }
+
+    val description: Observable<String>
+        get() = characterObservable.map { it.description }
+
+    val thumbnailUrl: Observable<String>
+        get() = characterObservable.map { it.thumbnail?.path + "." + it.thumbnail?.extension }
+
+    val characterCommand: Command
+        get() = asyncCharacterCommand
 
 }

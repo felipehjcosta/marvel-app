13ec834308712d533b0368d14b418bf7ec3d18bf
Bruno Kosawa <bruno.costa@olxbr.com>
2017-02-10 13:37:39 -0200

    Using ViewModel to populate detail view

--- app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/detail/view/DetailActivity.kt
+++ app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/detail/view/DetailActivity.kt
@@ -9,14 +9,10 @@ import android.support.v7.widget.Toolbar
 import android.widget.ImageView
 import android.widget.TextView
 import com.felipecosta.kotlinrxjavasample.R
-import com.felipecosta.kotlinrxjavasample.data.pojo.Character
-import com.felipecosta.kotlinrxjavasample.detail.datamodel.DetailDataModel
 import com.felipecosta.kotlinrxjavasample.detail.di.DetailComponent
 import com.felipecosta.kotlinrxjavasample.detail.presentation.CharacterDetailViewModel
 import com.felipecosta.kotlinrxjavasample.di.HasSubcomponentBuilders
 import com.nostra13.universalimageloader.core.ImageLoader
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class DetailActivity : AppCompatActivity() {
@@ -29,8 +25,6 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
-    @Inject
-    lateinit var dataModel: DetailDataModel
     @Inject
     lateinit var viewModel: CharacterDetailViewModel
     lateinit var toolbarLayout: CollapsingToolbarLayout
@@ -51,10 +45,14 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        dataModel.character(1009718)
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { character -> loadInfo(character) }
+        bind()
+    }
+
+    private fun bind() {
+        viewModel.name.subscribe { toolbarLayout.title = it }
+        viewModel.description.subscribe { description.text = it }
+        viewModel.thumbnailUrl.subscribe { imageLoader.displayImage(it, backdrop) }
+        viewModel.characterCommand.execute().subscribe()
     }
 
     private fun initView() {
@@ -68,12 +66,6 @@ class DetailActivity : AppCompatActivity() {
         description = findViewById(R.id.text_description) as TextView
 
         imageLoader = ImageLoader.getInstance()
-    }
-
-    private fun loadInfo(character: Character) {
-        toolbarLayout.title = character.name
-        imageLoader.displayImage(character.thumbnail?.path + "." + character.thumbnail?.extension, backdrop)
-        description.text = character.description
     }
 
 }

05f3819fa2d917446285d67cea33a28ec52ca802
fcostaa <felipe.h.j.costa@gmail.com>
2017-02-11 16:34:21 -0200

    Remove FAB from MainActivity

--- app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/main/MainActivity.kt
+++ app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/main/MainActivity.kt
@@ -2,29 +2,19 @@ package com.felipecosta.kotlinrxjavasample.main
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.design.widget.FloatingActionButton
-import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
 import com.felipecosta.kotlinrxjavasample.R
-import com.felipecosta.kotlinrxjavasample.detail.view.DetailActivity
 import com.felipecosta.kotlinrxjavasample.modules.listing.view.ListingFragment
 import com.felipecosta.kotlinrxjavasample.samples.Sample2Fragment
 import com.felipecosta.kotlinrxjavasample.samples.Sample3Fragment
-import com.felipecosta.kotlinrxjavasample.samples.Sample4Fragment
 import com.jakewharton.rxbinding2.support.design.widget.itemSelections
-import com.jakewharton.rxbinding2.view.clicks
 import io.reactivex.Observable.merge
 import io.reactivex.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var bottomNavigationView: BottomNavigationView
-
-    lateinit var fab: FloatingActionButton
 
     lateinit var disposable: Disposable
 
@@ -41,8 +31,6 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         bottomNavigationView = findViewById(R.id.nav_view) as BottomNavigationView
-
-        fab = findViewById(R.id.fab) as FloatingActionButton
     }
 
     private fun bind() {
@@ -68,19 +56,6 @@ class MainActivity : AppCompatActivity() {
         disposable = contentFragmentObservable.
                 filter { !(supportFragmentManager.findFragmentById(R.id.main_content)?.javaClass?.equals(it.javaClass) ?: false) }.
                 subscribe { supportFragmentManager.beginTransaction().replace(R.id.main_content, it).commit() }
-
-        val fabClickObservable = fab.clicks()
-
-        fabClickObservable.
-                subscribe {
-                    view ->
-                    handleClick(view)
-                }
-
-    }
-
-    private fun handleClick(view: Any?) {
-        DetailActivity.startDetail(this, 1009718)
     }
 
     override fun onDestroy() {

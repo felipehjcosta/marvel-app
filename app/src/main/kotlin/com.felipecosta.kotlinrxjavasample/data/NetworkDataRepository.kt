76a45e6cd87b1ec066a422a027c5ab948fb1ed0a
fcostaa <felipe.h.j.costa@gmail.com>
2017-05-26 00:52:27 -0300

    Replace Jake's RxJava2 Adapter by Retrofit

--- app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/data/NetworkDataRepository.kt
+++ app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/data/NetworkDataRepository.kt
@@ -2,11 +2,11 @@ package com.felipecosta.kotlinrxjavasample.data
 
 import com.felipecosta.kotlinrxjavasample.BuildConfig
 import com.felipecosta.kotlinrxjavasample.data.pojo.Character
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import io.reactivex.Observable
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.security.NoSuchAlgorithmException
 import java.util.*

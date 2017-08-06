1b3975de2f0e960fc0f4fcacc3eebfa975a66979
fcostaa <felipe.h.j.costa@gmail.com>
2017-08-06 13:01:10 -0300

    Rewrite the onCreateViewHolder method to be more Koltin idiomatic

--- app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/modules/listing/view/CharacterItemRecyclerViewAdapter.kt
+++ app/src/main/kotlin/com.felipecosta.kotlinrxjavasample/modules/listing/view/CharacterItemRecyclerViewAdapter.kt
@@ -4,7 +4,7 @@ import android.animation.AnimatorInflater
 import android.graphics.drawable.ColorDrawable
 import android.support.v4.content.res.ResourcesCompat.getColor
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
+import android.view.LayoutInflater.from
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -25,9 +25,9 @@ class CharacterItemRecyclerViewAdapter : RecyclerView.Adapter<CharacterItemRecyc
     val onItemSelected: Observable<Int>
         get() = onItemSelectedRelay
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.listing_fragment_item, parent, false)
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = from(parent.context)
+            .inflate(R.layout.listing_fragment_item, parent, false).run {
+        ViewHolder(this)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {

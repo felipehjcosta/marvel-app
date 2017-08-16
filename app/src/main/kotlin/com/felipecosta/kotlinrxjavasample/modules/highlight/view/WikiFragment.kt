35daeeea6c087a46e4637a8a9442c8024149fee9
fcostaa <felipe.h.j.costa@gmail.com>
2017-08-16 08:54:37 -0300

    Inject ViewModels in WikiFragment

--- app/src/main/kotlin/com/felipecosta/kotlinrxjavasample/modules/highlight/view/WikiFragment.kt
+++ app/src/main/kotlin/com/felipecosta/kotlinrxjavasample/modules/highlight/view/WikiFragment.kt
@@ -7,9 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.felipecosta.kotlinrxjavasample.R
+import com.felipecosta.kotlinrxjavasample.modules.highlight.presentation.HighlightCharacterViewModel
+import com.felipecosta.kotlinrxjavasample.modules.highlight.presentation.OthersCharactersViewModel
+import javax.inject.Inject
 
 
 class WikiFragment : Fragment() {
+
+    @Inject
+    lateinit var highlightCharacterViewModel: HighlightCharacterViewModel
+
+    @Inject
+    lateinit var othersCharactersViewModel: OthersCharactersViewModel
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {

96b0355738b7190c775aa648eaebd233977b432c
Felipe Costa <felipe.costa@olxbr.com>
2017-08-23 10:45:50 -0300

    Show others characters

--- app/src/main/kotlin/com/felipecosta/kotlinrxjavasample/modules/wiki/view/WikiFragment.kt
+++ app/src/main/kotlin/com/felipecosta/kotlinrxjavasample/modules/wiki/view/WikiFragment.kt
@@ -3,12 +3,18 @@ package com.felipecosta.kotlinrxjavasample.modules.wiki.view
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.felipecosta.kotlinrxjavasample.R
+import com.felipecosta.kotlinrxjavasample.modules.listing.view.CharacterItemRecyclerViewAdapter
 import com.felipecosta.kotlinrxjavasample.modules.wiki.presentation.HighlightedCharactersViewModel
 import com.felipecosta.kotlinrxjavasample.modules.wiki.presentation.OthersCharactersViewModel
+import com.felipecosta.kotlinrxjavasample.rx.findBy
+import com.felipecosta.kotlinrxjavasample.rx.plusAssign
+import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
 
@@ -20,9 +26,46 @@ class WikiFragment : Fragment() {
     @Inject
     lateinit var othersCharactersViewModel: OthersCharactersViewModel
 
+    lateinit var compositeDisposable: CompositeDisposable
+
+    lateinit var adapter: CharacterItemRecyclerViewAdapter
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_wiki, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view?.let {
+            val recyclerView: RecyclerView = it.findBy(R.id.others_characters_recycler_view)
+
+            recyclerView.layoutManager = GridLayoutManager(context, 3)
+            adapter = CharacterItemRecyclerViewAdapter()
+            recyclerView.adapter = adapter
+
+            bind()
+        }
+    }
+
+    private fun bind() {
+        compositeDisposable = CompositeDisposable()
+
+        compositeDisposable += othersCharactersViewModel.items
+                .doOnNext { adapter.replaceItems(it) }
+                .subscribe()
+
+        compositeDisposable += othersCharactersViewModel.loadItemsCommand.execute().subscribe()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        unbind()
+    }
+
+    private fun unbind() {
+        compositeDisposable.dispose()
     }
 
     companion object {

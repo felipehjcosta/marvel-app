f391c6b83ecf626ae66da8c25ec31a4198da3769
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-01-31 09:32:33 -0200

    Verify command was executed on CharacterListingFragment creation

--- app/src/test/kotlin/com/felipecosta/kotlinrxjavasample/modules/listing/view/CharacterListingFragmentTest.kt
+++ app/src/test/kotlin/com/felipecosta/kotlinrxjavasample/modules/listing/view/CharacterListingFragmentTest.kt
@@ -7,10 +7,12 @@ import com.felipecosta.kotlinrxjavasample.TestStubApplication
 import com.felipecosta.kotlinrxjavasample.modules.listing.presentation.CharacterItemViewModel
 import com.felipecosta.kotlinrxjavasample.modules.listing.presentation.CharacterListViewModel
 import com.felipecosta.kotlinrxjavasample.util.bindView
+import com.felipecosta.rxaction.RxCommand
 import io.mockk.every
 import io.mockk.mockk
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject.createDefault
+import io.reactivex.subjects.CompletableSubject
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -25,15 +27,17 @@ class CharacterListingFragmentTest {
 
     private val mockViewModel = mockk<CharacterListViewModel>(relaxed = true)
 
+    private val mockLoadItemsCommand = mockk<RxCommand<Any>>(relaxed = true)
+
     private val items = listOf(CharacterItemViewModel(
             1,
             "Thor",
             "http://i.annihil.us/u/prod/marvel/i/mg/d/d0/5269657a74350.jpg"))
 
+    private val fragment = CharacterListingFragment().apply { viewModel = mockViewModel }
+
     @Test
     fun givenItemsEmittedWhenStartFragmentItShouldPopulateTheList() {
-        val fragment = CharacterListingFragment()
-        fragment.viewModel = mockViewModel
         createDefault(items).apply {
             every { mockViewModel.items } returns this
         }
@@ -46,8 +50,6 @@ class CharacterListingFragmentTest {
 
     @Test
     fun givenShowLoadingEmittedWhenStartFragmentItShouldShowSwipeRefresh() {
-        val fragment = CharacterListingFragment()
-        fragment.viewModel = mockViewModel
         createDefault(items).apply {
             every { mockViewModel.showLoading } returns Observable.just(true)
         }
@@ -57,5 +59,17 @@ class CharacterListingFragmentTest {
         val swipeRefresh: SwipeRefreshLayout by fragment.bindView(R.id.listing_swipe_refresh)
 
         assertTrue { swipeRefresh.isRefreshing }
+    }
+
+    @Test
+    fun whenStartFragmentItShouldExecuteLoadItemsCommand() {
+        val commandExecuteCompletable = CompletableSubject.create().apply {
+            every { mockLoadItemsCommand.execute(any()) } returns this
+        }
+        every { mockViewModel.loadItemsCommand } returns mockLoadItemsCommand
+
+        startFragment(fragment)
+
+        assertTrue { commandExecuteCompletable.hasObservers() }
     }
 }

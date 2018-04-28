67f90f102877ab5b8d727bfaae7f6e1406619717
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-04-28 12:38:16 -0300

    Expose deeplink to main activity
    And also, refactor the deeplink navigation functions.

--- app-base/src/main/kotlin/com/github/felipehjcosta/marvelapp/base/util/NavUtils.kt
+++ app-base/src/main/kotlin/com/github/felipehjcosta/marvelapp/base/util/NavUtils.kt
@@ -2,20 +2,23 @@ package com.github.felipehjcosta.marvelapp.base.util
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.net.toUri
 
 fun navigateToDetail(activity: Activity, id: Int) {
-    val uri = "https://marvelapp.instantappsample.com/detail/${id}".toUri()
-    Intent(Intent.ACTION_VIEW, uri).apply {
-        addCategory(Intent.CATEGORY_BROWSABLE)
-        `package` = activity.packageName
-        activity.startActivity(this)
-    }
+    "https://marvelapp.instantappsample.com/detail/$id".toUri().startDeepLink(activity)
 }
 
 fun navigateToListing(activity: Activity) {
-    val uri = "https://marvelapp.instantappsample.com/listing".toUri()
-    Intent(Intent.ACTION_VIEW, uri).apply {
+    "https://marvelapp.instantappsample.com/listing".toUri().startDeepLink(activity)
+}
+
+fun navigateToMain(activity: Activity) {
+    "https://marvelapp.instantappsample.com/main".toUri().startDeepLink(activity)
+}
+
+private fun Uri.startDeepLink(activity: Activity) {
+    Intent(Intent.ACTION_VIEW, this).apply {
         addCategory(Intent.CATEGORY_BROWSABLE)
         `package` = activity.packageName
         activity.startActivity(this)

14d602f8ef2159768aee081f0e54af30a8d6cd52
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-07-15 11:38:00 -0300

    Remove universalimageloader stuff from test stub

--- feature/wiki/src/test/java/com/github/felipehjcosta/marvelapp/wiki/TestStubApplication.kt
+++ feature/wiki/src/test/java/com/github/felipehjcosta/marvelapp/wiki/TestStubApplication.kt
@@ -1,14 +1,10 @@
 package com.github.felipehjcosta.marvelapp.wiki
 
 import android.app.Application
-import com.nostra13.universalimageloader.core.ImageLoader
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
 
 class TestStubApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         setTheme(android.support.v7.appcompat.R.style.Theme_AppCompat_NoActionBar)
-        val config = ImageLoaderConfiguration.Builder(this).build()
-        ImageLoader.getInstance().init(config)
     }
 }

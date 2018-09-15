fc289d075603e52272fb37df8445651f1d6e5764
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-09-15 17:25:11 -0300

    Improve UI performance with RxJava
    More information here: https://medium.com/@sweers/rxandroids-new-async-api-4ab5b3ad3e93

--- feature/base/src/main/kotlin/com/github/felipehjcosta/marvelapp/base/DemoApplication.kt
+++ feature/base/src/main/kotlin/com/github/felipehjcosta/marvelapp/base/DemoApplication.kt
@@ -2,16 +2,17 @@ package com.github.felipehjcosta.marvelapp.base
 
 import android.app.Activity
 import android.app.Application
+import android.os.Looper
 import android.support.v4.app.Fragment
 import com.github.felipehjcosta.marvelapp.base.di.ApplicationComponent
-import com.github.felipehjcosta.marvelapp.base.util.AppInjector
 import com.github.felipehjcosta.marvelapp.base.di.DaggerApplicationComponent
-import com.nostra13.universalimageloader.core.ImageLoader
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
+import com.github.felipehjcosta.marvelapp.base.util.AppInjector
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import dagger.android.support.HasSupportFragmentInjector
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
 open class DemoApplication : Application(), HasActivityInjector, HasSupportFragmentInjector {
@@ -26,9 +27,16 @@ open class DemoApplication : Application(), HasActivityInjector, HasSupportFragm
 
     override fun onCreate() {
         super.onCreate()
+        setupAndroidSchedulers()
         component = createComponent()
         component.inject(this)
         AppInjector.init(this)
+    }
+
+    private fun setupAndroidSchedulers() {
+        val asyncMainThreadScheduler = AndroidSchedulers.from(Looper.getMainLooper(), true)
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { asyncMainThreadScheduler }
+        RxAndroidPlugins.setMainThreadSchedulerHandler { asyncMainThreadScheduler }
     }
 
     protected open fun createComponent(): ApplicationComponent {

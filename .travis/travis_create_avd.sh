f1ae5e7ffaf8be4cc394566213a55bca134489de
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-09-16 22:42:15 -0300

    Use avdmanager to create emulator

--- .travis/travis_create_avd.sh
+++ .travis/travis_create_avd.sh
@@ -4,8 +4,7 @@
 # Avoid the tempation to skip the boot animation. CI scripts use that to figure out when the device is ready.
 # The emulator is started in the background. This is to avoid holding up the build until we need it.
 
-android-update-sdk --components=sys-img-v7a-android-21 --accept-licenses='android-sdk-license-[0-9a-f]{8}'
-echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
+$HOME/android-sdk/tools/bin/avdmanager create avd -n test -k "system-images;android-21;default;armeabi-v7a"
 emulator -avd test -no-skin -no-audio -no-window -gpu off &
 
 exit 0

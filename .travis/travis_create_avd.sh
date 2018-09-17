9c298c26ab4ae35f6fab59b98ef1e0abb9d7ee7a
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-09-16 22:47:46 -0300

    Pipe "echo no" first to android emulator creation command

--- .travis/travis_create_avd.sh
+++ .travis/travis_create_avd.sh
@@ -4,7 +4,7 @@
 # Avoid the tempation to skip the boot animation. CI scripts use that to figure out when the device is ready.
 # The emulator is started in the background. This is to avoid holding up the build until we need it.
 
-$HOME/android-sdk/tools/bin/avdmanager create avd -n test -k "system-images;android-21;default;armeabi-v7a"
+echo no | $HOME/android-sdk/tools/bin/avdmanager create avd -n test -k "system-images;android-21;default;armeabi-v7a"
 emulator -avd test -no-skin -no-audio -no-window -gpu off &
 
 exit 0

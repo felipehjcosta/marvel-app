7b1d6bcbdbbddfd1f2c9623eff636b93ffc7ea1e
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-11-15 14:23:01 -0200

    Bump Kotlin and related dependencies

--- library/network/src/main/kotlin/com/github/felipehjcosta/marvelapp/network/utils/NetworkUtils.kt
+++ library/network/src/main/kotlin/com/github/felipehjcosta/marvelapp/network/utils/NetworkUtils.kt
@@ -1,6 +1,6 @@
 package com.github.felipehjcosta.marvelapp.network.utils
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.stringBased
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.serializationConverterFactory
 import kotlinx.serialization.json.JSON
 import okhttp3.MediaType
 import retrofit2.Converter
@@ -9,5 +9,5 @@ fun createNetworkConverterFactory(
         contentType: MediaType = MediaType.parse("application/json")!!,
         json: JSON = JSON.nonstrict
 ): Converter.Factory {
-    return stringBased(contentType, json::parse, json::stringify)
+    return serializationConverterFactory(contentType, json)
 }

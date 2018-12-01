dc32ee50c77015c228aaf895fe3f788f8ae8381a
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-12-01 00:08:12 -0200

    Extract all library/plugins/dependencies versions to buildSrc

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,48 @@
+object Versions {
+    const val kotlin = "1.3.10"
+
+    const val compileSdkVersion = 28
+    const val buildToolsVersion = "28.0.3"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 28
+
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    const val supportLibrary = "28.0.0"
+    const val constraintLayout = "1.1.3"
+
+    const val roomLibrary = "1.1.1"
+
+    const val rxJavaLibrary = "2.2.4"
+    const val rxJavaAndroidLibrary = "2.1.0"
+    const val rxJavaRelayLibrary = "2.1.0"
+    const val rxJavaBindingLibrary = "2.2.0"
+    const val rxActionLibrary = "1.1.0"
+
+    const val androidxLibrary = "0.2"
+
+    const val okhttpLibrary = "3.12.0"
+    const val retrofitLibrary = "2.5.0"
+    const val kotlinSerializationConverterLibrary = "0.2.0"
+
+    const val daggerLibrary = "2.19"
+    const val javaxAnnotationLibrary = "1.0"
+
+    const val imageLoaderLibrary = "1.9.5"
+
+    const val galleryLayoutManagerLibrary = "1.1.0"
+    const val recyclerViewDslLibrary = "0.7.0"
+
+    const val junitLibrary = "4.12"
+    const val mockkLibrary = "1.8.13.kotlin13"
+    const val robolectricLibrary = "3.7.1"
+
+    const val espressoLibrary = "3.1.0"
+    const val androidTestLibrary = "1.1.0"
+    const val androidTestJunitLibrary = "1.0.0"
+    const val androidTestTruthLibrary = "1.0.0"
+    const val kakaoLibrary = "1.4.0"
+    const val rx2IdlerLibrary = "0.9.0"
+    const val testButlerLibrary = "1.3.1"
+}

ebb0a214bcbddb44e0e28caca0dba2af1d690f54
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-12-04 00:46:39 -0200

    Rewrite unit-test-utils library build script with kotlin DSL

---
+++ library/unit-test-utils/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(Versions.compileSdkVersion)
+    buildToolsVersion(Versions.buildToolsVersion)
+
+    defaultConfig {
+        minSdkVersion(Versions.minSdkVersion)
+        targetSdkVersion(Versions.targetSdkVersion)
+        versionCode = Versions.versionCode
+        versionName = Versions.versionName
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    lintOptions {
+        isAbortOnError = false
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+        getByName("test").java.srcDirs("src/test/kotlin")
+        getByName("androidTest").java.srcDirs("src/androidTest/kotlin")
+    }
+}
+
+dependencies {
+    implementation(SupportDependencies.appCompat)
+    KotlinDependencies.values.forEach { implementation(it) }
+}

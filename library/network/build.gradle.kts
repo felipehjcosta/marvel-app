d9812bff4294041de82def34e7364c84e7ff7513
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-12-04 09:14:32 -0200

    Rewrite network library build script with kotlin DSL

---
+++ library/network/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Versions.compileSdkVersion)
+    buildToolsVersion(Versions.buildToolsVersion)
+
+    defaultConfig {
+        minSdkVersion(Versions.minSdkVersion)
+        targetSdkVersion(Versions.targetSdkVersion)
+        versionCode = Versions.versionCode
+        versionName = Versions.versionName
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isTestCoverageEnabled = true
+        }
+        create("debugMini") {
+            initWith(getByName("debug"))
+            matchingFallbacks = listOf("debug")
+        }
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+        getByName("test").java.srcDirs("src/test/kotlin")
+        getByName("androidTest").java.srcDirs("src/androidTest/kotlin")
+    }
+
+}
+
+dependencies {
+    KotlinDependencies.values.forEach { implementation(it) }
+
+    NetworkDependencies.values.forEach { implementation(it) }
+}
+
+val add_test_dependencies: groovy.lang.Closure<Any?> by extra
+add_test_dependencies(project)

c920ed7d03651a492745203fae0c52d18a3c41a0
felipehjcosta <felipe.h.j.costa@gmail.com>
2019-01-15 09:49:16 -0200

    Bump AGP

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@ internal object Kotlin {
 }
 
 object Versions {
-    const val androidGradlePlugin = "3.2.1"
+    const val androidGradlePlugin = "3.3.0"
     const val dependencyGraphGeneratorPlugin = "0.2.0"
     const val androidJunitJacocoPlugin = "0.13.0"
     const val detektGradlePlugin = "1.0.0-RC12"

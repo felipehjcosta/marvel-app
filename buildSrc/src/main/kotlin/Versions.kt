b00c2e09d9e72271c7044332e0c6c40dd190fd4a
felipehjcosta <felipe.h.j.costa@gmail.com>
2019-09-12 23:15:46 -0300

    Bump detekt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val androidGradlePlugin = "3.5.0"
     const val dependencyGraphGeneratorPlugin = "0.2.0"
     const val androidJunitJacocoPlugin = "0.13.0"
-    const val detektGradlePlugin = "1.0.0-RC12"
+    const val detektGradlePlugin = "1.0.1"
     const val canidropjetifierPlugin = "0.4"
 
     const val compileSdkVersion = 28
